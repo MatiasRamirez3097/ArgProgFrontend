a58419a81a129924d4c84a3db745ad9feffcba0c
Matias Ramirez <matiasgr3009@gmail.com>
2022-10-25 19:57:28 -0300

    solved minor bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    url: 'http://localhost:8080/',
-    //url: 'https://backendmgr.herokuapp.com/'
+    //url: 'http://localhost:8080/',
+    url: 'https://backendmgr.herokuapp.com/'
 };
 
 /*
